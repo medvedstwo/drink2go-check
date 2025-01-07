3036af4720eeabb9df8e06f0febcb918c00d3c66
Михаил Терешин <medvedstwo@gmail.com>
2025-01-07 18:16:19 +0300

    feat: добавляет библиотеку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,34 @@
 /* в этот файл добавляет скрипты*/
+
+const slider = document.getElementById('slider');
+const rangeInputMin = document.getElementById('range-input-min');
+const rangeInputMax = document.getElementById('range-input-max');
+
+noUiSlider.create(slider, {
+  start: [0, 900],
+  connect: true,
+  range: {
+    'min': 0,
+    'max': 1000
+  },
+  cssPrefix: 'range__element-'
+});
+
+slider.noUiSlider.on('update', (values, handle) => {
+
+  const value = values[handle];
+
+  if (handle) {
+    rangeInputMax.value = Math.round(value);
+  } else {
+    rangeInputMin.value = Math.round(value);
+  }
+});
+
+rangeInputMin.addEventListener('change', function () {
+  slider.noUiSlider.set([this.value, null]);
+});
+
+rangeInputMax.addEventListener('change', function () {
+  slider.noUiSlider.set([null, this.value]);
+});
